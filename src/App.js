ee3807802aa46efff44dc7ab48e80adfeb6852e8
Muhamad Raafi <muhamad.raafi@codr.id>
2022-10-19 16:03:43 +0700

    route done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Login from './pages/login';
 import Berita from './pages/berita';
 
 function App() {
+  const tokenData = localStorage.getItem('token');
+  console.log(tokenData)
   return (
-    <div>
-      {/* <Login /> */}
-      <Berita />
-    </div>
+    <>
+      {
+        tokenData !== null ? (
+          <Router>
+            <Switch>
+              <Route path="/" component={Berita} />
+              <Route path="/Berita" component={Berita} />
+              <Route component={Berita} />
+            </Switch>
+          </Router >
+        ) : (
+          <Router>
+            <Switch>
+              <Route path="/" component={Login} />
+              <Route component={Login} />
+            </Switch>
+          </Router >
+        )
+      }
+    </>
   );
 }
 
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,15 +1,31 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
 const Login = () => {
+    const history = useHistory();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = () => {
+        if (email === 'hengki@gmail.com' && password === 'test123') {
+            localStorage.setItem('token', '1');
+            history.push('/');
+        } else {
+            alert('password salah coba gunakan email hengki@gmail.com dan password: test123')
+        }
+    }
+
     return (
         <div className="vw-100 vh-100 d-flex justify-content-center align-items-center">
             <div className="card p-3">
-                <form className="d-flex flex-column gap-3">
+                <form className="d-flex flex-column gap-3" onSubmit={handleSubmit}>
                     <div className="form-group">
                         <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                        <input type="email" value={email} onChange={e => setEmail(e.target.value)} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
                     </div>
                     <div className="form-group">
                         <label for="exampleInputPassword1">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                        <input type="password" value={password} onChange={e => setPassword(e.target.value)} className="form-control" id="exampleInputPassword1" placeholder="Password" />
                     </div>
                     <button type="submit" className="btn btn-primary">Sign In</button>
                 </form>
